5a168a146abbbf8dd20d9c3156483c59850c22ef
TheOnlySmartBoy <o2jose43@gmail.com>
2020-03-21 09:46:26 +0300

    Updated routes adding all required front-end routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','PagesController@index')->name('index');
+Route::get('/about','PagesController@about')->name('about');
+Route::get('/gallery','PagesController@gallery')->name('galery');
+Route::get('/campaigns','PagesController@campaign')->name('campaign');
+Route::get('/volunteer','PagesController@volunteer')->name('volunteer');
+Route::get('/contact','PagesController@showContactPage')->name('contact');
+Route::post('/contact','PagesController@contact')->name('contact');
 
 Auth::routes();
 
